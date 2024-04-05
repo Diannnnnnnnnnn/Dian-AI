773ab36564828a2b3c39a8767050edf78782f143
Diannnnnnnnnnn <137965921+Diannnnnnnnnnn@users.noreply.github.com>
2024-04-05 16:34:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,36 +10,37 @@ def save_knowledge_base(file_path: str, data: dict):
   with open(file_path, 'w') as file:
     json.dump(data, file, indent=2)
 
-def find_best_match(user_question:str, questions: list[str]) → str | None:
-  matches: list = get_close_matches(use_question, questions, n=1, cutoff=8.6)
-  return matches[0] if matches else None
+def find_best_match(user_question: str, questions: list[str]) -> str | None:
+    matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.8)
+    return matches[0] if matches else None
 
-def get_answer_for_question (question: str, knowledge_base: dict) → str | None:
-  for q in knowledge_base ["questions"]:
-    if q["question"] = question:
-      return q["answer"]
+def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
+    for q in knowledge_base["questions"]:
+        if q["question"] == question:
+            return q["answer"]
       
 def chat_bot():
-  knowledge_base: dict load_knowledge_base('knowledge_base.json')
+    knowledge_base = load_knowledge_base('knowledge_base.json')
 
-  while True:
-    user_input: str = input('You:')
+    while True:
+        user_input = input('You: ')
 
-    if user_input.lower() = 'quit':
-      break
+        if user_input.lower() == 'quit':
+            break
 
-    best_match: str | None find_best_match(user_input, [q["question"] for q in knowledge_base("questions"]])
+        best_match = find_best_match(user_input, [q["question"] for q in knowledge_base["questions"]])
 
-    if best_match:
-    answer: str = get_answer_for_question(best_match, knowledge_base) print(f'Bot: {answer}')
-    else:
-      print('Bot: I don\'t know the answer. Can you teach me?')
-      new_answer: str = input('')
+        if best_match:
+            answer = get_answer_for_question(best_match, knowledge_base)
+            print(f'Bot: {answer}')
+        else:
+            print('Bot: I don\'t know the answer. Can you teach me?')
+            new_answer = input('')
 
-     if new_answer.lower() ≠ 'skip':
-       knowledge_base["questions"].append({"question": user_input, "answer": new_answer})
-       save_knowledge_base('knowledge_base.json', knowledge_base)
-       print('Bot: Thank you! I learned a new response)
+            if new_answer.lower() != 'skip':
+                knowledge_base["questions"].append({"question": user_input, "answer": new_answer})
+                save_knowledge_base('knowledge_base.json', knowledge_base)
+                print('Bot: Thank you! I learned a new response')
 
-if __init__ == __main__:
-     chatbot()
+if __name__ == '__main__':
+    chat_bot()
